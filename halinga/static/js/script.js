567440fbbe721cbb41016682f10d1e5df648fba5
sseong <73373959+sseong08@users.noreply.github.com>
2024-07-16 09:57:34 +0900

    Add files via upload

---
+++ halinga/static/js/script.js
@@ -0,0 +1,24 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".tab-button").click();
+});
+
+document.getElementById("search-form").addEventListener("submit", function() {
+    document.getElementById("loading-overlay").style.display = "flex";
+});
