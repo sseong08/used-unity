7dd3a3909ca93308e82ac2dc4726a17ebbe79fe9
sseong <73373959+sseong08@users.noreply.github.com>
2024-07-22 18:24:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -85,11 +85,11 @@ def lightning(plusurl):
 def index():
     if request.method == 'POST':
         query = request.form['query']
+        print(query)
         jun_results = junggo(query)
         bun_results = lightning(query)
         all_results = sorted(jun_results + bun_results, key=lambda x: int(x['price'].replace(',', '').replace('원', '').replace(' ', '')))
         return render_template('index.html', query=query, jun_results=jun_results, bun_results=bun_results, all_results=all_results)
-        print(query)
     return render_template('index.html')
 
 if __name__ == '__main__':
